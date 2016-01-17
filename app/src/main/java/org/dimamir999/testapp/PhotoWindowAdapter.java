52a9de7733a0dd34a21c45d65137afa4c6702824
dimamir999 <dimamir999@gmail.com>
2016-01-17 22:58:48 +0200

    initial commit

---
+++ app/src/main/java/org/dimamir999/testapp/PhotoWindowAdapter.java
@@ -0,0 +1,22 @@
+package org.dimamir999.testapp;
+
+import android.view.View;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Created by dimamir999 on 17.01.16.
+ */
+public class PhotoWindowAdapter implements GoogleMap.InfoWindowAdapter{
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+}
