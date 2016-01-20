9dcd6438c16e823c8623a01def8ea69012571410
dimamir999 <dimamir999@gmail.com>
2016-01-20 03:44:25 +0200

    add loading photo from url

--- app/src/main/java/org/dimamir999/testapp/view/PhotoFragment.java
+++ app/src/main/java/org/dimamir999/testapp/view/PhotoFragment.java
@@ -1,18 +1,32 @@
 package org.dimamir999.testapp.view;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import org.dimamir999.testapp.R;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 public class PhotoFragment extends Fragment{
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_photo, container, false);
     }
+
+
 }
--- app/src/main/java/org/dimamir999/testapp/view/PickPhotoFragment.java
+++ app/src/main/java/org/dimamir999/testapp/view/PickPhotoFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -13,11 +14,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.dimamir999.testapp.R;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by dimamir999 on 19.01.16.
@@ -30,11 +37,20 @@ public class PickPhotoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_photo_picker, container, false);
         Button fromGalleryButton = (Button) view.findViewById(R.id.from_gallery_button);
+        Button fromURLButton = (Button) view.findViewById(R.id.from_url_button);
+        final TextView urlField = (TextView) view.findViewById(R.id.url_field);
         fromGalleryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.v("dimamir999", "handle click on from gallery button");
                 loadPhotoFromGallery();
+            }
+        });
+        fromURLButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("dimamir999", "handle click on from URL button");
+                loadPhotoFromURL(urlField.getText().toString());
             }
         });
         return view;
@@ -48,9 +64,8 @@ public class PickPhotoFragment extends Fragment {
     }
 
     public void loadPhotoFromURL(String url) {
-        Bitmap photo = null;
-        InputStream stream = null;
-        photo = BitmapFactory.decodeStream(stream);
+        ImageLoader imageLoader = new ImageLoader();
+        imageLoader.execute("http://s005.radikal.ru/i212/1601/cf/ce10c779036fx.jpg");
     }
 
     @Override
@@ -83,5 +98,63 @@ public class PickPhotoFragment extends Fragment {
     public void setPhotoToView(Bitmap photo){
         ImageView imageView = (ImageView) getActivity().findViewById(R.id.photoView);
         imageView.setImageBitmap(photo);
+    }
+
+    public class ImageLoader extends AsyncTask<String, Void, Bitmap> {
+
+        private static final int OK = 0;
+        private static final int BIG_FILE = 1;
+        private static final int INCORRECT_FORMAT = 2;
+        private static final int MAX_SIZE = 20 * 1024;
+
+        private int answerCode;
+
+        private ImageView photoView = (ImageView) getActivity().findViewById(R.id.photoView);
+        private ProgressBar uploadProgressBar = (ProgressBar) getActivity().findViewById(R.id.upload_progress_bar);
+
+        @Override
+        protected void onPreExecute() {
+            uploadProgressBar.setVisibility(ProgressBar.VISIBLE);
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                URLConnection connection = url.openConnection();
+                if(connection.getContentLength() > MAX_SIZE){
+                    answerCode = BIG_FILE;
+                    return null;
+                } else {
+                    Bitmap photo = BitmapFactory.decodeStream(connection.getInputStream());
+                    if(photo == null)
+                        answerCode = INCORRECT_FORMAT;
+                    else {
+                        answerCode = OK;
+                    }
+                    return photo;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            uploadProgressBar.setVisibility(ProgressBar.INVISIBLE);
+            switch (answerCode){
+                case OK:
+                    photoView.setImageBitmap(result);
+                    break;
+                case BIG_FILE:
+                    Toast.makeText(getActivity(), "File size more than max size" + MAX_SIZE, Toast.LENGTH_LONG).show();
+                    break;
+                case INCORRECT_FORMAT:
+                    Toast.makeText(getActivity(), "Incorrect file format", Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
+
     }
 }
