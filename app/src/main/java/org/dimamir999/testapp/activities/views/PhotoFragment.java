64b4edfcf2c551fd89a09c257e79e188da8d6eff
dimamir999 <dimamir999@gmail.com>
2016-01-20 19:22:56 +0200

    add saving photo when configuration changed

--- app/src/main/java/org/dimamir999/testapp/activities/views/PhotoFragment.java
+++ app/src/main/java/org/dimamir999/testapp/activities/views/PhotoFragment.java
@@ -1,20 +1,39 @@
 package org.dimamir999.testapp.activities.views;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import org.dimamir999.testapp.R;
 
 
 public class PhotoFragment extends Fragment{
 
+    private Bitmap currentPhoto;
+    private ImageView imageView;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_photo, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_photo, container, false);
+        imageView = (ImageView)view.findViewById(R.id.photoView);
+        if(savedInstanceState != null)
+            imageView.setImageBitmap(savedInstanceState.<Bitmap>getParcelable("photo"));
+        return view;
+    }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable("photo", ((BitmapDrawable)imageView.getDrawable()).getBitmap());
+    }
 }
